bdc19b4f31730e2e0fa1af77650c1a1e03afb5a9
dbyun89@gmail.com <byun.d@husky.neu.edu>
2015-12-09 00:44:15 -0500

    adding equipment manipulation pages part 14: one last try

--- project/server/app.js
+++ project/server/app.js
@@ -2,6 +2,6 @@ module.exports = function(app, mongoose, db) {
     var model = require("./models/page.model.js")(mongoose, db);
     require("./services/page.service.server.js")(app, model);
 	
-    var equipmentModel = require("./models/equipment.model.js")(mongoose, db);
-    require("./services/equipment.service.server.js")(app, equipmentModel);
+/*     var equipmentModel = require("./models/equipment.model.js")(mongoose, db);
+    require("./services/equipment.service.server.js")(app, equipmentModel); */
 };
