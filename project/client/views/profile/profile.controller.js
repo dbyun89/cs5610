c6b126128d3f3fc82c5ae63f5ae78a427aacadd3
dbyun89@gmail.com <byun.d@husky.neu.edu>
2015-12-16 16:41:01 -0500

    Trying something in profile.controller.js

--- project/client/views/profile/profile.controller.js
+++ project/client/views/profile/profile.controller.js
@@ -4,8 +4,8 @@
 		.controller("ProfileController", ProfileController);
 
 	function ProfileController(UserService) {
-		var vm = this;
-		vm.update = update;
+		var model = this;
+		model.update = update;
     
 		function update(user) {
 			UserService.update(user, function(response) {
